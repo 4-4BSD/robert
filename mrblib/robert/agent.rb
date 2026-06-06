4d163df8d897e2449d098c066fc51f595f5d0c49
robert <0x1eef@hardenedbsd.org>
2026-06-06 13:02:18 +0000

    agent: add docs, move things around

--- mrblib/robert/agent.rb
+++ mrblib/robert/agent.rb
@@ -13,12 +13,17 @@ module Robert
 
     attr_accessor :ui
 
+    ##
+    # @return [Task::Queue]
     def queue
       stream.task_queue
     end
 
+    ##
+    # @param [LLM::Function] tool
+    # @param [Symbol] strategy
+    # @return [LLM::Function::Return]
     def on_tool_confirmation(tool, strategy)
-      raise "Agent UI is not configured" unless ui
       Widgets::Confirmation.new(ui, tool).confirm(strategy)
     end
 
@@ -27,7 +32,13 @@ module Robert
     ##
     # @return [Array<String>]
     def tools
-      Robert.disable_confirmations? ? [] : ["read-file", "grep", "find"]
+      Robert.disable_confirmations? ? [] : filesystem_tools
+    end
+
+    ##
+    # @return [Array<String>]
+    def filesystem_tools
+      ["read-file", "grep", "find"]
     end
   end
 end
