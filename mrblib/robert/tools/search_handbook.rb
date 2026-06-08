c5f8f2135a42bd3afde1beae87b5900e8bc861df
robert <0x1eef@hardenedbsd.org>
2026-06-08 13:47:52 +0000

    handbook: update endpoint

--- mrblib/robert/tools/search_handbook.rb
+++ mrblib/robert/tools/search_handbook.rb
@@ -4,12 +4,18 @@ module Robert::Tools
   class SearchHandbook < LLM::Tool
     name "search-handbook"
     description "Search the FreeBSD handbook with full-text search"
-    parameter :keyword, String, "The keyword to search for"
-    required %i[keyword]
+    parameter :q, String, "Tne search query"
+    required %i[q]
 
-    def call(keyword:)
-      res = Curl.get("https://4.4bsd.dev", params: {q: keyword})
+    def call(q:)
+      res = Curl.get(endpoint, params: {q:})
       JSON.parse(res.body)
+    end
+
+    private
+
+    def endpoint
+      "https://4.4bsd.dev/search"
     end
   end
 end
